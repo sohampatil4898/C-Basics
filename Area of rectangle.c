d20914f0ae5358eaf0fde35cab77657738338d62
Soham patil <sohampatil4898@gmail.com>
2026-08-09 22:43:30 +0530

    Add files via upload

---
+++ Area of rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ int main(){
+    int length,width;
+    printf("enter the length:");
+    scanf("%d",&length);
+    printf("enter the width:");
+    scanf("%d",&width);
+    printf("Area of rectangle:%d",length*width);
+    return 0;
+ }
+ 
