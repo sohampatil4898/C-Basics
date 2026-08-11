b5c3eec27205d7d00926d9967b2f63c387290827
Soham patil <sohampatil4898@gmail.com>
2026-08-11 12:27:50 +0530

    Create Average.c

---
+++ Average.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+ int main(){
+    int num1,num2,num3;
+     printf("enter the number:");
+     scanf("%d,%d,%d",&num1,&num2,&num3);
+     printf("Average:%d",(num1+num2+num3)/3);
+     return 0;
+ }
