c8456eee8b6fc934cff067b9a621d87da128e898
Soham patil <sohampatil4898@gmail.com>
2026-08-11 13:36:41 +0530

    Create adult or not.c

---
+++ adult or not.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int age;
+    printf("Enter the age:");
+    scanf("%d",&age);
+    if(age>18){
+        printf("adult\n");
+    }else{
+        printf("not adult:\n");
+    }
+    return 0;
+    
+}
